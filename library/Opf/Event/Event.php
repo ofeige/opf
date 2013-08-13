f66bd6a6c3d317d369147cbdca800f5a4cf74752
Oliver Feige <oliver@feige.net>
2013-08-13 22:57:44 +0200

    New Event Handler

---
+++ library/Opf/Event/Event.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Opf\Event;
+
+class Event implements EventInterface
+{
+   private $name;
+   private $context;
+   private $info;
+   private $cancelled = false;
+
+   public function __construct($name, $context = null, $info = null)
+   {
+      $this->name = $name;
+      $this->context = $context;
+      $this->info = $info;
+   }
+
+   public function getName()
+   {
+      return $this->name;
+   }
+
+   public function getContext()
+   {
+      return $this->context;
+   }
+
+   public function getInfo()
+   {
+      return $this->info;
+   }
+
+   public function isCancelled()
+   {
+      return $this->cancelled;
+   }
+
+   public function cancel()
+   {
+      $this->cancelled = true;
+   }
+}
---
+++ library/Opf/Event/EventInterface.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Opf\Event;
+
+interface EventInterface
+{
+   public function getName();
+   public function cancel();
+   public function isCancelled();
+}
