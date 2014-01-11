ea93ede76d608adadbf90cb181b06a7ccc037d14
Oliver Feige <ofeige@gmx.de>
2014-01-11 18:25:33 +0100

    Fix for Twig working

--- library/Opf/Template/ViewTwig.php
+++ library/Opf/Template/ViewTwig.php
@@ -4,64 +4,68 @@ namespace Opf\Template;
 
 use Opf\Http\RequestInterface;
 use Opf\Http\ResponseInterface;
+use Twig_Loader_Filesystem;
+use Twig_Environment;
+
+
 
 class ViewTwig implements ViewInterface
 {
 
-   private $template;
-   private $vars = array();
-   private $helpers = array();
+    private $template;
+    private $vars = array();
+    private $helpers = array();
 
-   public function __construct($template)
-   {
-      $this->template = $template;
-   }
+    public function __construct($template)
+    {
+        $this->template = $template;
+    }
 
-   public function assign($name, $value)
-   {
-      $this->vars[$name] = $value;
-   }
+    public function assign($name, $value)
+    {
+        $this->vars[$name] = $value;
+    }
 
-   public function render(RequestInterface $request, ResponseInterface $response)
-   {
-       $loader = new Twig_Loader_Filesystem(OPF_APPLICATION_PATH . "/views/");
-       $twig = new Twig_Environment($loader);
-       $data = $twig->render("{$this->template}.twig", array('name' => 'Fabien'));
-       $response->write($data);
-   }
+    public function render(RequestInterface $request, ResponseInterface $response)
+    {
+        $loader = new Twig_Loader_Filesystem(OPF_APPLICATION_PATH . "/views/");
+        $twig = new Twig_Environment($loader, array('debug'=> true));
+        $twig->addExtension(new \Twig_Extension_Debug());
+        $data = $twig->render("{$this->template}.twig", $this->vars);
+        $response->write($data);
+    }
 
-   public function __get($property)
-   {
-      if (isset($this->vars[$property])) {
-         return $this->vars[$property];
-      }
+    public function __get($property)
+    {
+        if (isset($this->vars[$property])) {
+            return $this->vars[$property];
+        }
 
-      return null;
-   }
+        return null;
+    }
 
-   public function __call($methodName, $args)
-   {
-      $helper = $this->loadViewHelper($methodName);
-      $val = $helper->execute($args);
+    public function __call($methodName, $args)
+    {
+        $helper = $this->loadViewHelper($methodName);
+        $val = $helper->execute($args);
 
-      return $val;
-   }
+        return $val;
+    }
 
-   protected function loadViewHelper($helper)
-   {
-      $helperName = ucfirst($helper);
-      if (!isset($this->helpers[$helper])) {
-         $className = "{$helperName}";
-         $fileName = OPF_APPLICATION_PATH . "/views/helpers/{$className}.php";
-         if (file_exists($fileName) === false) {
-            throw new \Exception("File not found {$fileName}");
-         }
-         include_once $fileName;
-         $this->helpers[$helper] = new $className();
-      }
+    protected function loadViewHelper($helper)
+    {
+        $helperName = ucfirst($helper);
+        if (!isset($this->helpers[$helper])) {
+            $className = "{$helperName}";
+            $fileName = OPF_APPLICATION_PATH . "/views/helpers/{$className}.php";
+            if (file_exists($fileName) === false) {
+                throw new \Exception("File not found {$fileName}");
+            }
+            include_once $fileName;
+            $this->helpers[$helper] = new $className();
+        }
 
-      return $this->helpers[$helper];
-   }
+        return $this->helpers[$helper];
+    }
 }
 
-?>
