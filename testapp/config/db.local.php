d98b86ec3f2167040038ed5926964e2f45079be1
Oliver Feige <ofeige@gmx.de>
2014-01-11 13:44:48 +0100

    Add support for Twig View

---
+++ testapp/config/db.local.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: feige
+ * Date: 29.06.13
+ * Time: 22:25
+ * To change this template use File | Settings | File Templates.
+ */
+return array(
+    
+);
