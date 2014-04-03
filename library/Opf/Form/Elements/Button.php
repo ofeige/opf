b9350d311887988991b7074beec3cd2ae5f5a479
Oliver Feige <feige@Olivers-MacBook-Pro.local>
2014-04-03 23:57:38 +0200

    Add Support for Values to Buttons

--- library/Opf/Form/Elements/Button.php
+++ library/Opf/Form/Elements/Button.php
@@ -1,33 +1,30 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: feige
- * Date: 28.01.14
- * Time: 22:22
- */
 
-namespace Opf\Form\Elements;
+    namespace Opf\Form\Elements;
 
 
-use Opf\Form\ElementInterface;
+    use Opf\Form\ElementAbstract;
+    use Opf\Form\ElementInterface;
 
-class Button implements ElementInterface
-{
-    protected $label = '';
-
-    public function __construct($label)
+    class Button extends ElementAbstract implements ElementInterface
     {
-        $this->label = $label;
-    }
+        protected $label = '';
 
-    public function __toString()
-    {
-        $html = '<div class="form-group">
+        public function __construct($name, $label, $value)
+        {
+            $this->name  = $name;
+            $this->label = $label;
+            $this->value = $value;
+        }
+
+        public function __toString()
+        {
+            $html = '<div class="form-group">
     <div class="col-sm-offset-2 col-sm-10">
-        <button type="submit" class="btn btn-primary">%s</button>
+        <button type="submit" class="btn btn-primary" name="%s" value="%s">%s</button>
     </div>
 </div>';
 
-        return sprintf($html, $this->label);
+            return sprintf($html, $this->name, $this->value, $this->label);
+        }
     }
-} 
