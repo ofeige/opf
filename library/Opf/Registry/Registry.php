3cf9875c1fed6918155c8c080bbf1e6e06cfb39b
Oliver Feige <ofeige@gmx.de>
2014-04-06 13:47:53 +0200

    add logger to registry

--- library/Opf/Registry/Registry.php
+++ library/Opf/Registry/Registry.php
@@ -8,91 +8,104 @@ use Opf\Session\SessionInterface;
 
 class Registry
 {
-   protected static $instance = null;
-   protected $values = array();
+    protected static $instance = null;
+    protected $values = array();
 
-   const KEY_REQUEST = 'request';
-   const KEY_RESPONSE = 'response';
-   const KEY_SESSION = 'session';
+    const KEY_REQUEST  = 'request';
+    const KEY_RESPONSE = 'response';
+    const KEY_SESSION  = 'session';
+    const KEY_LOGGER   = 'logger';
 
-   /**
-    * return instance of Registry
-    *
-    * @return Registry
-    */
-   public static function getInstance()
-   {
-      if (self::$instance === null) {
-         self::$instance = new Registry();
-      }
-      return self::$instance;
-   }
+    /**
+     * return instance of Registry
+     *
+     * @return Registry
+     */
+    public static function getInstance()
+    {
+        if (self::$instance === null) {
+            self::$instance = new Registry();
+        }
 
-   protected function __construct()
-   {
-   }
+        return self::$instance;
+    }
 
-   private function __clone()
-   {
-   }
+    protected function __construct()
+    {
+    }
 
-   protected function set($key, $value)
-   {
-      $this->values[$key] = $value;
-   }
+    private function __clone()
+    {
+    }
 
-   protected function get($key)
-   {
-      if (isset($this->values[$key])) {
-         return $this->values[$key];
-      }
-      return null;
-   }
+    protected function set($key, $value)
+    {
+        $this->values[$key] = $value;
+    }
 
-   public function setRequest(RequestInterface $request)
-   {
-      $this->set(self::KEY_REQUEST, $request);
-   }
+    protected function get($key)
+    {
+        if (isset($this->values[$key])) {
+            return $this->values[$key];
+        }
 
-   public function setResponse(ResponseInterface $response)
-   {
-      $this->set(self::KEY_RESPONSE, $response);
-   }
+        return null;
+    }
 
-   public function setSession(SessionInterface $session)
-   {
-      $this->set(self::KEY_SESSION, $session);
-   }
+    public function setRequest(RequestInterface $request)
+    {
+        $this->set(self::KEY_REQUEST, $request);
+    }
 
-   /**
-    * return IRequest Klasse
-    *
-    * @return IRequest
-    */
-   public function getRequest()
-   {
-      return $this->get(self::KEY_REQUEST);
-   }
+    public function setResponse(ResponseInterface $response)
+    {
+        $this->set(self::KEY_RESPONSE, $response);
+    }
 
-   /**
-    * return IResponse Klasse
-    *
-    * @return IResponse
-    */
-   public function getResponse()
-   {
-      return $this->get(self::KEY_RESPONSE);
-   }
+    public function setSession(SessionInterface $session)
+    {
+        $this->set(self::KEY_SESSION, $session);
+    }
 
-   /**
-    * return ISession Klasse
-    *
-    * @return SessionInterface
-    */
-   public function getSession()
-   {
-      return $this->get(self::KEY_SESSION);
-   }
+    public function setLogger(\Psr\Log\LoggerInterface $logger)
+    {
+        $this->set(self::KEY_LOGGER, $logger);
+    }
+
+    /**
+     * return IRequest Klasse
+     *
+     * @return IRequest
+     */
+    public function getRequest()
+    {
+        return $this->get(self::KEY_REQUEST);
+    }
+
+    /**
+     * return IResponse Klasse
+     *
+     * @return IResponse
+     */
+    public function getResponse()
+    {
+        return $this->get(self::KEY_RESPONSE);
+    }
+
+    /**
+     * return ISession Klasse
+     *
+     * @return SessionInterface
+     */
+    public function getSession()
+    {
+        return $this->get(self::KEY_SESSION);
+    }
+
+    public function getLogger()
+    {
+        return $this->get(self::KEY_LOGGER);
+    }
 }
 
 ?>
