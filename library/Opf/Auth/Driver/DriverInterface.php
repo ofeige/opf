dfba4f5a03aec197250a52837e917ddf2f0b5483
Oliver Feige <ofeige@gmx.de>
2014-04-20 21:54:51 +0200

    todo was on the wrong place

--- library/Opf/Auth/Driver/DriverInterface.php
+++ library/Opf/Auth/Driver/DriverInterface.php
@@ -9,8 +9,6 @@ interface DriverInterface
      * @param string $password
      * @param array $group A list of group names to validate the User
      * @return bool
-     *
-     * @todo    implement the group feature for php arrays
      */
     public function isValid($username, $password, array $group = array());
 }
